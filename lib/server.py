a606e984cef61dba39b2ddca3897ab300d209464
Jordan <jordan.wallace.williams@gmail.com>
2020-05-23 14:52:16 -0600

    Create Python Flask server

---
+++ lib/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'teamawesome'
+
+socketIo = SocketIO(app, cors_allowed_origins="*")
+
+app.debug = True
+
+app.host = 'localhost'
+
+@socketIo.on("message")
+def handleMessage(msg):
+  print(msg)
+  send(msg, broadcast=True)
+  return None
+
+if __name__ == '__main__':
+    socketIo.run(app)
