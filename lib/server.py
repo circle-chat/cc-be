89180e18828a3a2854cbe7aeac6db99f86998279
Ryan Allen <rcallen89@gmail.com>
2020-05-26 12:51:55 -0500

    Modify server file for different socket events

--- lib/server.py
+++ lib/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from flask_socketio import SocketIO, send, emit, join_room, leave_room, rooms
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -8,44 +8,43 @@ app.config['SECRET_KEY'] = 'teamawesome'
 app.debug = True
 app.host = 'localhost'
 
-# Recieve Message. Namespace is an example
-@socketio.on('message', namespace='/group/test')
-def recieve_message(message):
-    print('received message: ' + message)
 
 # Send Message. Namespace is an example
 @socketio.on('message')
-def send_message(message):
-    send(message, namespace='/group/test')
+def send_message(data):
+  room = data['room']
+  msg = data['message']
+  # print(data['message'], room=room)
+  send(msg, room=room)
+  
 
 # Join a room. `on_join` expects a dictionary argument.
 @socketio.on('join')
 def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
+  room = data['room']
+  join_room(room)
+  send(f'Welcome to the {room} room', room=room)
+  # timer.limit = close_room()
+
 
 # Leave a room. `on_leave` expect a dictionary argument.
 @socketio.on('leave')
 def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
+  room = data['room']
+  leave_room(room)
+  send(f'A User left the {room} room', room=room)
+
 
 # Global Broadcast.
-@socketio.on("message")
+@socketio.on("gmessage")
 def handleMessage(msg):
-  print(msg)
   send(msg, broadcast=True)
-  return None
 
 # Error handling default.
 @socketio.on_error_default
 def default_error_handler(e):
-    print(request.event["message"]) # "my error event"
-    print(request.event["args"])    # (data,)
+  print(request.event["message"]) # "my error event"
+  print(request.event["args"])    # (data,)
 
 if __name__ == '__main__':
     socketio.run(app)
