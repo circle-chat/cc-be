ec70e756c3f746a802b7e16d17fa660d9fe42dc0
David Atkins <atki1080@gmail.com>
2020-05-27 00:59:00 -0600

    Add connection model

--- database/models.py
+++ database/models.py
@@ -3,3 +3,7 @@ from .mongodb import db
 class Group(db.Document):
   access_code = db.StringField(required=True, unique=True)
   description = db.StringField(required=True)
+
+class Connection(db.Document):
+  sid = db.StringField(require=True, unique=True)
+  group = db.StringField(require=True)
