e4dd33d662a43cf54a514be765809443910c72c2
Jordan <jordan.wallace.williams@gmail.com>
2020-05-28 23:34:19 -0600

    Update Connection model to include created field, and meta field to auto-expire data

--- lib/models.py
+++ lib/models.py
@@ -20,3 +20,13 @@ class Group(me.Document):
 class Connection(me.Document):
   sid = me.StringField(require=True, unique=True)
   group = me.StringField(require=True)
+  meta = {
+      'auto_create_index': True,
+      'index_opts': {'expireAfterSeconds': 259200},
+      'indexes': [
+          {
+              'fields': ['created'],
+              'expireAfterSeconds': 259200
+          }
+      ]
+  }
