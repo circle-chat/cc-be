f73665b0e612ab9b04ff2de15854406de6751b4c
Jordan <jordan.wallace.williams@gmail.com>
2020-06-01 11:25:16 -0600

    Add from flask_cors import CORS. Add CORS(app)

--- circle.py
+++ circle.py
@@ -3,9 +3,10 @@ from config import Config
 from flask_socketio import SocketIO
 from flask_mongoengine import MongoEngine
 from os import environ
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('config.ProdConfig')
 db = MongoEngine(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
