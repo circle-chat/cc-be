f982be1ab382ad2a6ec39c4fe2a9e317b367f2cf
Jordan <jordan.wallace.williams@gmail.com>
2020-05-31 23:28:54 -0600

    For now comment out all code. This is to allow a different Python app structure

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from config import Config
-from flask_socketio import SocketIO
-from flask_mongoengine import MongoEngine
+# from flask import Flask
+# from config import Config
+# from flask_socketio import SocketIO
+# from flask_mongoengine import MongoEngine
 
 
-app = Flask(__name__)
-app.config.from_object('config.ProdConfig')
-db = MongoEngine(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+# app = Flask(__name__)
+# app.config.from_object('config.ProdConfig')
+# db = MongoEngine(app)
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
-from lib import routes, models
+# from lib import routes, models
