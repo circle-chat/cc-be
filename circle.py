65279a4482c0a00f622c5fab22a5b88bffbd175e
Jordan <jordan.wallace.williams@gmail.com>
2020-05-31 23:32:08 -0600

    Update circle.py to be the primary app. This is to improve structure for production deployment

--- circle.py
+++ circle.py
@@ -1,1 +1,16 @@
-from lib import app
+from flask import Flask
+from config import Config
+from flask_socketio import SocketIO
+from flask_mongoengine import MongoEngine
+from os import environ
+
+
+app = Flask(__name__)
+app.config.from_object('config.ProdConfig')
+db = MongoEngine(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+from lib import routes, models
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 8080)))
